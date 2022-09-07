c924dbbc2bfceb97e7cfc34e94ab210f3101d31d
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-09-07 12:05:21 +0530

    Add project files.

---
+++ DelegatesDemo/EventDelegatePerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelegatesLibrary;
+namespace DelegatesDemo
+{
+    class EventDelegatePerson
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Walk(100);
+
+            Console.Read();
+
+            
+
+        }
+    }
+}
---
+++ DelegatesLibrary/customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesLibrary
+{
+    public class Customer
+    {
+        public int custid { get; set; }
+        public string custname { get; set; }
+        public int phoneno { get; set; }
+    }
+}
